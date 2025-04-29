7dd4b4b905267ab76862cab78dd50b7591055704
Balaaditya <mbalaaditya@gmail.com>
2025-04-29 11:08:02 +0000

    Modified parse.sh

--- parse.sh
+++ parse.sh
@@ -1,9 +1,7 @@
 take=$1
-file=""
-tr -d "\r" < "$take" > file
-if [[ -s file ]]; then
-sed -f update.sed file | awk '{print $0;}' > upfile
-awk 'BEGIN{
+if [[ -s "$take" ]]; then
+tr -d "\r" < "$take" | sed -f update.sed | awk '
+BEGIN{
     FS=" ";
 }
 {
@@ -24,16 +22,15 @@ END{
             print event[i]
         }
     }
-}' upfile > events 
+}' > events 
     if (($? != 0)); then
         echo "File not eligible for converting into csv"
         exit 1
     else
-        paste -d"," file events | sed -e 's/\] /,/g' -e 's/\[//g' -e 's/\(client .*\),D/[\1] D/' > upfile
-        sed -f reupdate.sed upfile | awk 'BEGIN{ FS=",";OFS=","} {print $4}' > events
-        paste -d"," upfile events > final.csv
+        paste -d"," <(tr -d "\r" < "$take") events | sed -e 's/\] /,/g' -e 's/\[//g' -e 's/\(client .*\),D/[\1] D/' > upfile
+        sed -f reupdate.sed upfile | awk 'BEGIN{ FS=",";OFS=","} {print $4}' | paste -d"," upfile - > final.csv
     fi
-    rm events upfile file
+    rm events upfile
 else
     echo "Empty file!"
 fi
